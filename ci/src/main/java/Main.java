5fa5bf672e51677779c92f93f60be14fedcb16dd
moise <moise@kth.se>
2021-02-09 08:49:40 +0100

    Progress on skeleton

--- ci/src/main/java/Main.java
+++ ci/src/main/java/Main.java
@@ -1,3 +1,19 @@
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+import java.io.IOException;
+
+/*
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+*/
+/**
+ Skeleton of a ContinuousIntegrationServer which acts as webhook
+ See the Jetty documentation for API documentation of those classes.
+ */
+
 public class Main {
     public static void main(String[] args){
         System.out.println("l");
