cb098169984e9c7ab133120f08c4c1dd80caed10
Johan <johennin@kth.se>
2021-02-10 14:55:08 +0100

    add some more changes #10 pair programming with Ioana

--- src/main/java/HistoryManager.java
+++ src/main/java/HistoryManager.java
@@ -1,8 +1,29 @@
+import com.google.gson.Gson;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class HistoryManager {
 
     public HistoryManager() {
 
+    }
+
+    public void writeBuildResultToFile(String repositoryName, HistoryEntry historyEntry) throws IOException {
+        String filePath = "history/" + repositoryName + ".json";
+        File file = new File(filePath);
+        Gson gson = new Gson();
+        FileWriter writer;
+        if (!file.exists()) {
+            //Files.createDirectory();
+            writer = new FileWriter(filePath);
+        } else {
+            writer = new FileWriter(filePath, true);
+            writer.append("\n");
+        }
+
+        gson.toJson(historyEntry, writer);
+        writer.close();
     }
 
 
