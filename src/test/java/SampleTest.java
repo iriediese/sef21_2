db35e6331a3b2a50d2b764b46dcd3b33510b706d
moise <moise@kth.se>
2021-02-10 15:19:54 +0100

    Fix #19, fix #20

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,2 +1,13 @@
-public class Test {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+    @Test
+    public void trueTest(){
+        Assert.assertTrue(1 == 1);
+    }
+    @Test
+    public void falseTest(){
+        Assert.assertFalse(1 == 0);
+    }
 }
